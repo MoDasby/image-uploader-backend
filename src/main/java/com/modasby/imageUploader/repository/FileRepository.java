cbe1a5bed7c4b2e0c839f2032970280e43ec2064
MoDasby <giullianomendes033@gmail.com>
2022-01-10 17:37:20 -0300

    removido imports desnecessarios

--- src/main/java/com/modasby/imageUploader/repository/FileRepository.java
+++ src/main/java/com/modasby/imageUploader/repository/FileRepository.java
@@ -4,8 +4,6 @@ import com.modasby.imageUploader.model.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface FileRepository extends JpaRepository<File, String> {
 }
